58c0f0f731ef37076b09eab98771711be77eae37
Atif Shahzad <zarkosportswear@gmail.com>
2025-10-20 12:38:22 +0500

    Add scroller smooth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -7,6 +10,26 @@ import ChatbotWidget from './components/ChatbotWidget'
 import ScrollTopButton from './components/ScrollTopButton'
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t) => 1 - Math.pow(1 - t, 1.5),
+      smoothWheel: true,
+      smoothTouch: false
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Header />
